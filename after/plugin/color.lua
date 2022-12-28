ff05dc2ba37e763c52ff32f402d6739f37668521
Geoffrey Noel <gnoel5@bloomberg.net>
2022-12-28 15:08:18 -0500

    Add rose-pine colorscheme and restructure

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,7 +1,18 @@
 -- vim.cmd[[colorscheme gruvbox]]
 
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
-vim.g.background = "dark"
+-- vim.g.tokyonight_transparent_sidebar = true
+-- vim.g.tokyonight_transparent = true
+-- vim.g.background = "dark"
+-- 
+-- vim.cmd[[colorscheme tokyonight-moon]]
 
-vim.cmd[[colorscheme tokyonight-moon]]
+function ColorMyPencils(color)
+    color = color or 'rose-pine'
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
+end
+
+ColorMyPencils()
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,106 +9,42 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 
 return require('packer').startup({
     function(use)
-        ---------------------
-        -- Package Manager --
-        ---------------------
-
         use('wbthomason/packer.nvim')
-
-        ----------------------
-        -- Required plugins --
-        ----------------------
 
         use('nvim-lua/plenary.nvim')
 
-        ------------------
-        -- Color Scheme --
-        ------------------
-
-        use('gruvbox-community/gruvbox')
-        use('folke/tokyonight.nvim')
+        -- use('gruvbox-community/gruvbox')
+        -- use('folke/tokyonight.nvim')
+        use({
+                'rose-pine/neovim',
+                as = 'rose-pine',
+        })
 
         use('nvim-tree/nvim-web-devicons')
 
-        -----------------------------------
-        -- Treesitter: Better Highlights --
-        -----------------------------------
-
         use({
-            {
                 'nvim-treesitter/nvim-treesitter',
-                event = 'CursorHold',
                 run = ':TSUpdate',
-                config = function()
-                    require('plugins.treesitter')
-                end,
-            },
-            { 'nvim-treesitter/playground', after = 'nvim-treesitter' },
-            { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' },
-            { 'nvim-treesitter/nvim-treesitter-refactor', after = 'nvim-treesitter' },
-            { 'windwp/nvim-ts-autotag', after = 'nvim-treesitter' },
-            { 'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter' },
         })
 
-        ---------------------------------
-        -- Navigation and Fuzzy Search --
-        ---------------------------------
-
-        use({
-            {
-                'nvim-telescope/telescope.nvim',
-                event = 'CursorHold',
-                config = function()
-                    require('plugins.telescope')
-                end,
-            },
-            {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                after = 'telescope.nvim',
-                run = 'make',
-                config = function()
-                    require('telescope').load_extension('fzf')
-                end,
-            },
-            {
-                'nvim-telescope/telescope-symbols.nvim',
-                after = 'telescope.nvim',
-            },
-        })
-
-        ---------------------
-        -- Navigation tree --
-        ---------------------
+        use('nvim-telescope/telescope.nvim')
 
         use({
                 'nvim-tree/nvim-tree.lua',
                 requires = {
-                    'nvim-tree/nvim-web-devicons'    
+                    'nvim-tree/nvim-web-devicons'
                 },
                 cmd = { 'NvimTreeToggle', 'NvimTreeClose' },
                 config = function()
                     require('plugins.nvim-tree')
                 end,
-            }
-        )
-
-        -------------
-        -- Haskell --
-        -------------
+            })
 
         use('neovimhaskell/haskell-vim')
 
-        ----------------
-        -- ToggleTerm --
-        ----------------
-        --
-
         use({
                 'akinsho/toggleterm.nvim', 
-                tag = '*', 
-                config = function()
-                    require("plugins.toggleterm")
-                end,
+                tag = '*',
             }
         )
 
